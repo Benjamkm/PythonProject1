e5600b5cf1970a741dcc927be2cb29d5457e1395
Benjamkm <benni.majasalmi1@gmail.com>
2026-01-15 16:50:18 +0200

    added teht 5

---
+++ Moduuli  2/teht 5.py
@@ -0,0 +1,11 @@
+talents = float(input("Enter talents: "))
+pounds = float(input("Enter pounds: "))
+lots = float(input("Enter lots: "))
+pounds_per_talent = 20
+lots_per_pound = 32
+grams_per_lot = 13.3
+total_grams = (talents*pounds_per_talent*lots_per_pound*grams_per_lot+pounds*lots_per_pound*grams_per_lot+lots*grams_per_lot)
+kilograms = int(total_grams //1000)
+remaining_grams = total_grams - kilograms *1000
+print("The weight in modern units:")
+print(f"{kilograms} kilograms and {remaining_grams:.2f} grams.")
