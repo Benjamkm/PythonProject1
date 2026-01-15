3e3370c3f1425e5fc9836c22e7b56f8f9c4bf943
Benjamkm <benni.majasalmi1@gmail.com>
2026-01-15 18:45:37 +0200

    teht 6

---
+++ Moduuli  2/teht 6.py
@@ -0,0 +1,12 @@
+import random
+yksi = random.randint(0, 9)
+kaksi = random.randint(0, 9)
+kolme = random.randint(0, 9)
+
+neljä = random.randint(1, 6)
+viisi = random.randint(1, 6)
+kuusi = random.randint(1, 6)
+seitsemän = random.randint(1, 6)
+
+print(f"3-digit code: {yksi}{kaksi}{kolme}")
+print(f"4-digit code: {neljä}{viisi}{kuusi}{seitsemän}")
