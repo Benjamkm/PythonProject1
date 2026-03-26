40f28642d8b3a99ca887d01f7904b17ab6d45e63
Benjamkm <benni.majasalmi1@gmail.com>
2026-03-26 13:53:08 +0200

    moduuli 11 teht 1 ja 2

---
+++ Moduuli 11/teht 2.py
@@ -0,0 +1,54 @@
+import random
+
+class Car:
+    def __init__(self, license_plate, maximum_speed):
+        self.license_plate = license_plate
+        self.maximum_speed = maximum_speed
+        self.current_speed = 0
+        self.travelled_distance = 0
+
+    def accelerate(self, change):
+        self.current_speed += change
+
+        if self.current_speed > self.maximum_speed:
+            self.current_speed = self.maximum_speed
+        if self.current_speed < 0:
+            self.current_speed = 0
+
+    def drive(self, hours):
+        self.travelled_distance += self.current_speed * hours
+
+
+class Race:
+    def __init__(self, name, distance, cars):
+        self.name = name
+        self.distance = distance
+        self.cars = cars
+
+    def hour_passes(self):
+        for car in self.cars:
+            change = random.randint(-10, 15)
+            car.accelerate(change)
+            car.drive(1)
+
+    def print_status(self):
+        print(f"{'Car':<10} {'Max km/h':<10} {'Speed':<10} {'Distance':<10}")
+        for car in self.cars:
+            print(f"{car.license_plate:<10} "
+                  f"{car.maximum_speed:<10} "
+                  f"{car.current_speed:<10} "
+                  f"{car.travelled_distance:<10}")
+
+    def race_finished(self):
+        for car in self.cars:
+            if car.travelled_distance >= self.distance:
+                return True
+        return False
+class ElectricCar(Car):
+    def __init__(self, license_plate, maximum_speed, battery_capacity):
+        super().__init__(license_plate, maximum_speed)
+        self.battery_capacity = battery_capacity
+class GasolineCar(Car):
+    def __init__(self, license_plate, maximum_speed, tank_volume):
+        super().__init__(license_plate, maximum_speed)
+        self.tank_volume = tank_volume
